45ba320115f9cdf4eb0ba21c5072511bdf4cc0e5
Diya Mishra <msdiyamishra@gmail.com>
2023-06-26 15:27:08 -0400

    Initial Commit

---
+++ hellooooo/helloooooApp.swift
@@ -0,0 +1,17 @@
+//
+//  helloooooApp.swift
+//  hellooooo
+//
+//  Created by Scholar on 6/26/23.
+//
+
+import SwiftUI
+
+@main
+struct helloooooApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
